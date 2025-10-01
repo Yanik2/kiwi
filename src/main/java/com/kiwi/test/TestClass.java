b00b5d122cc06d9aef92cbcbeb97bb78c9a7347f
Yanik2 <wm_yanik@ukr.net>
2025-10-01 10:36:44 +0200

    add test class

---
+++ src/main/java/com/kiwi/test/TestClass.java
@@ -0,0 +1,13 @@
+package com.kiwi.test;
+
+public class TestClass {
+    private final String testName;
+
+    public TestClass(String testName) {
+        this.testName = testName;
+    }
+
+    public void execute() {
+        System.out.println("asdf");
+    }
+}
