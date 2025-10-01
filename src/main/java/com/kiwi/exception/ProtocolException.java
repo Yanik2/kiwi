6d500edd58d9528c11fc125ebb08706da30e0820
Yan Zinchenko <65082029+Yanik2@users.noreply.github.com>
2025-10-01 13:03:36 +0200

    develop/socket_lifecycle_timeouts (#4)
    * develop/socket_lifecycle_timeouts
    
    * develop/socket_lifecycle_timeouts -> add exception handling
    
    * develop/socket_lifecycle_timeouts -> refactor exception handling in handle request

--- src/main/java/com/kiwi/exception/ProtocolException.java
+++ src/main/java/com/kiwi/exception/ProtocolException.java
@@ -4,4 +4,8 @@ public class ProtocolException extends RuntimeException {
     public ProtocolException(String message) {
         super(message);
     }
+
+    public ProtocolException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/main/java/com/kiwi/server/RequestHandler.java
+++ src/main/java/com/kiwi/server/RequestHandler.java
@@ -1,13 +1,14 @@
 package com.kiwi.server;
 
 import static com.kiwi.server.Method.EXT;
-import static com.kiwi.util.Constants.CMD_EXT;
+import static com.kiwi.server.util.ServerConstants.ERROR_MESSAGE;
 
 import com.kiwi.dto.TCPRequest;
 import com.kiwi.dto.TCPResponse;
-import com.kiwi.exception.RequestParsingException;
+import com.kiwi.exception.ProtocolException;
 import java.io.InputStream;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -26,21 +27,26 @@ public class RequestHandler {
     }
 
     public void handle(Socket socket) {
-        try {
-            final InputStream is = socket.getInputStream();
-            while (true) {
-                final TCPRequest request = requestParser.parse(is);
-                final TCPResponse response = requestDispatcher.dispatch(request);
-                responseWriter.writeResponse(socket, response);
-                if (EXT.equals(request.method())) {
-                    break;
+        try (socket) {
+            try {
+                final InputStream is = socket.getInputStream();
+                while (true) {
+                    final TCPRequest request = requestParser.parse(is);
+                    final TCPResponse response = requestDispatcher.dispatch(request);
+                    responseWriter.writeResponse(socket, response);
+                    if (EXT.equals(request.method())) {
+                        break;
+                    }
                 }
+            } catch (ProtocolException e) {
+                log.log(Level.SEVERE, "Unexpected problem with protocol: " + e.getMessage());
+                responseWriter.writeResponse(socket, new TCPResponse(ERROR_MESSAGE, false));
             }
-
-            socket.close();
-        } catch (Exception ex) {
-            log.log(Level.SEVERE, "Unexpected error on get input stream");
-            throw new RequestParsingException("Unexpected exception", ex);
+        } catch (SocketTimeoutException e) {
+            log.log(Level.WARNING, "Socket timed out");
+        } catch (Exception e) {
+            log.log(Level.WARNING, "Unexpected exception during request processing: "
+                + e.getMessage());
         }
     }
 }
