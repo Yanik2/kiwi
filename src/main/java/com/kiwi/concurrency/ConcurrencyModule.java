749ffb674bfe78f11ba15f5868a74f68df446c8b
Yan Zinchenko <65082029+Yanik2@users.noreply.github.com>
2026-01-16 20:28:33 +0100

    develop/phase-3/remove dcl from concurrent module (#20)
    * develop/phase-3/remove dcl from concurrent module
    
    * develop/phase-3/remove init call from main

--- src/main/java/com/kiwi/concurrency/ConcurrencyModule.java
+++ src/main/java/com/kiwi/concurrency/ConcurrencyModule.java
@@ -7,7 +7,6 @@ import static com.kiwi.concurrency.util.Constants.THREAD_NAME_PREFIX;
 public final class ConcurrencyModule {
     private static KiwiThreadPool rejectionThreadPool;
     private static final String REJECTION_THREAD_POOL_NAME = "rejection-thread-pool";
-    private static final Object SYNC_OBJECT = new Object();
 
     public static KiwiThreadPoolExecutor createExecutor(String threadPoolExecutorName,
                                                         String threadPoolName,
@@ -22,23 +21,22 @@ public final class ConcurrencyModule {
                 threadPoolSize,
                 queueCapacity,
                 metrics,
-                rejectionThreadPool);
+                getRejectionThreadPool());
         metrics.registerMetrics();
         return executor;
     }
 
-    public static void init() {
+    private synchronized static KiwiThreadPool getRejectionThreadPool() {
         if (rejectionThreadPool == null) {
-            synchronized (SYNC_OBJECT) {
-                if (rejectionThreadPool == null) {
-                    final var kiwiThreadFactory = new KiwiThreadFactory(THREAD_NAME_PREFIX);
-                    final var metrics = ObservabilityModule.getThreadPoolMetrics(REJECTION_THREAD_POOL_NAME);
-                    rejectionThreadPool = new KiwiThreadPool(kiwiThreadFactory, REJECTION_THREAD_POOL_NAME,
-                            1, 100, metrics);
-                    metrics.registerMetrics();
-                    rejectionThreadPool.start();
-                }
-            }
+            final var kiwiThreadFactory = new KiwiThreadFactory(THREAD_NAME_PREFIX);
+            final var metrics = ObservabilityModule.getThreadPoolMetrics(REJECTION_THREAD_POOL_NAME);
+            metrics.registerMetrics();
+            final var rejectionTp =  new KiwiThreadPool(kiwiThreadFactory, REJECTION_THREAD_POOL_NAME,
+                    1, 100, metrics);
+            rejectionTp.start();
+            rejectionThreadPool = rejectionTp;
         }
+
+        return rejectionThreadPool;
     }
 }
